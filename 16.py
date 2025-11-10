026b9b578849b179fbb700a3a29c96c988fa8a7f
NamelessNanashi <53353250+NanashiTheNameless@users.noreply.github.com>
2025-11-09 23:29:30 -0600

    Add one I forgot

---
+++ 16.py
@@ -0,0 +1,3 @@
+# SPDX-License-Identifier: JSPL
+# Intentionally cursed. Do not read. Do not teach. Do not reuse.
+(lambda __:(__(__import__('builtins'),[112,114,105,110,116]))((lambda e,s:bytes.fromhex(''.join({v:k for k,v in e.items()}[c] for c in s)).decode())(dict(zip('0123456789abcdef','😀😁😂🤣😃😄😅😆😉😊😎😍😘🥰🥵🥶')),''.join(dict(zip('0123456789abcdef','😀😁😂🤣😃😄😅😆😉😊😎😍😘🥰🥵🥶'))[c] for c in '48656c6c6f2c20576f726c64'))))(lambda b,n:getattr(b,''.join(map(chr,n))))
